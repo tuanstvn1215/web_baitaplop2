55d76cdd855c0cf100cf4038c02fa5b799bc9fa9
tuanstvn1215 <tuanstvn1215@gmail.com>
2020-12-31 13:12:26 +0700

    .

---
+++ admin/block/menu.php
@@ -0,0 +1,41 @@
+<div id='right-menu' class="right-menu">
+    <div class="menu-icon-container">
+        <div class="menu-icon" onclick="right_menu()"><i class=" fas fa-bars"></i></div>
+    </div>
+    <div id='menu-content' class="menu-content">
+
+        <div class="menu-group">
+            <a href="<?= host ?>/admin/tatcadonhang.php">
+                <div class="menu-titl"><i class="fas fa-file-invoice"></i> Đơn đặt hàng</div>
+            </a>
+            <div class="menu-item"></div>
+            <div class="menu-item"></div>
+            <div class="menu-item"></div>
+        </div>
+        <div class="menu-group">
+
+            <a href="<?= host ?>/admin/tatcahanghoa.php">
+                <div class="menu-titl"> <i class="fas fa-box-open"></i> Hàng hóa</div>
+            </a>
+            <div class="menu-item"></div>
+            <div class="menu-item"></div>
+            <div class="menu-item"></div>
+            <div class="menu-item"></div>
+        </div>
+
+        <div class="menu-group"><a href="<?= host ?>/admin/tatcanhanvien.php">
+                <div class="menu-titl"> <i class="fas fa-user-friends"></i> Nhân viên</div>
+            </a>
+            <div class="menu-item"></div>
+            <div class="menu-item"></div>
+            <div class="menu-item"></div>
+        </div>
+        <div class="menu-group"><a href="<?= host ?>/admin/tatcakhachhang.php">
+                <div class="menu-titl"> <i class="fas fa-user-friends"></i> Khách hàng</div>
+            </a>
+            <div class="menu-item"> </div>
+            <div class="menu-item"></div>
+            <div class="menu-item"></div>
+        </div>
+    </div>
+</div>
